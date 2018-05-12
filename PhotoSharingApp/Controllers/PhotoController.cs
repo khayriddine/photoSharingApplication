1a90ddf3f37722069dca5be5b86a43d4fa9f711d
khayri <khayribattikh@gmail.com>
2018-05-12 03:11:27 -0700

    First Page Photo

---
+++ PhotoSharingApp/Controllers/PhotoController.cs
@@ -0,0 +1,19 @@
+﻿using PhotoSharingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhotoSharingApp.Controllers
+{
+    public class PhotoController : Controller
+    {
+        // GET: Photo
+        public ActionResult Index()
+        {
+            var photo = new Photo();
+            return View(photo);
+        }
+    }
+}
