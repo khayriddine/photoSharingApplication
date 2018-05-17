e72b6e55a9ac4d51f272de65ec2236ab79f27a0f
khayri <khayribattikh@gmail.com>
2018-05-17 06:51:13 -0700

    lab 4 exercie 1

---
+++ PhotoSharingApp/Controllers/PhotoController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Globalization;
+using PhotoSharingApp.Model;
+using System.Net;
+using System.IO;
+
+namespace PhotoSharingApp.Controllers
+{
+    public class PhotoController : Controller
+    {
+        private PhotoSharingContext context = new PhotoSharingContext();
+        // GET: Photo
+        public ViewResult Index()
+        {
+            return View("Index",context.Photos.ToList().ElementAt(0));
+        }
+
+        public ActionResult Display(int? id)
+        {
+            
+            Photo photo = context.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Display", photo);
+        }
+
+        public ActionResult Create()
+        {
+            Photo photo = new Model.Photo();
+            photo.CreatedDate = DateTime.Today;
+            return View("Create", photo);
+        }
+        [HttpPost]
+        public ActionResult Create(Photo photo, HttpPostedFileBase image)
+        {
+            photo.CreatedDate = DateTime.Today;
+            if (ModelState.IsValid)
+            {
+                context.Photos.Add(photo);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                if(image != null)
+                {
+                    photo.ImageMimeType = image.ContentType;
+                    photo.PhotoFile = new byte[image.ContentLength];
+                    image.InputStream.Read(photo.PhotoFile,0,image.ContentLength);
+                    
+                }
+
+                context.Photos.Add(photo);
+                context.SaveChanges();
+                return View("Create", photo);
+            }
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = context.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Delete",photo);
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photo photo = context.Photos.Find(id);
+            context.Photos.Remove(photo);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public FileContentResult GetImage(int? id)
+        {
+            Photo photo = context.Photos.Find(id);
+            if(photo != null)
+            {
+                
+                return   File(photo.PhotoFile, photo.ImageMimeType);
+            }
+
+            return null;
+            
+
+        }
+
+
+    }
+}
--- PhotoSharingApp/Model/PhotoSharingInitializer.cs
+++ PhotoSharingApp/Model/PhotoSharingInitializer.cs
@@ -18,13 +18,15 @@ namespace PhotoSharingApp.Model
             MyPhoto1.Description= "khayri";
             MyPhoto1.CreatedDate = DateTime.Now;
             MyPhoto1.PhotoFile = GeneralFunctions.getPhotoFileByName("C:/CSharp/p1/photoSharingApplication/Images/khayri.jpg");
+            MyPhoto1.ImageMimeType = "image/jpg";
             Photos.Add(MyPhoto1);
             context.SaveChanges();
             Photo MyPhoto2 = new Photo();
-            MyPhoto2.Owner = "Khayri";
-            MyPhoto2.Title = "khayri";
+            MyPhoto2.Owner = "hcen";
+            MyPhoto2.Title = "hcen";
             MyPhoto2.CreatedDate = DateTime.Now;
             MyPhoto2.PhotoFile = GeneralFunctions.getPhotoFileByName("C:/CSharp/p1/photoSharingApplication/Images/hcen.jpg");
+            MyPhoto2.ImageMimeType = "image/jpg";
             Photos.Add(MyPhoto2);
             context.Photos.AddRange(Photos);
             context.SaveChanges();
