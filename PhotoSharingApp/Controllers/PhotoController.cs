3b5e82030a6fc3b85da0bf732e3812a961c9e9ad
khayri <khayribattikh@gmail.com>
2018-05-17 11:01:47 -0700

    Optional—Writing the Action Filters in a Controller

--- PhotoSharingApp/Controllers/PhotoController.cs
+++ PhotoSharingApp/Controllers/PhotoController.cs
@@ -10,6 +10,7 @@ using System.IO;
 
 namespace PhotoSharingApp.Controllers
 {
+    [ValueReporter]
     public class PhotoController : Controller
     {
         private PhotoSharingContext context = new PhotoSharingContext();
---
+++ PhotoSharingApp/Controllers/ValueReporter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PhotoSharingApp.Controllers
+{
+    public class ValueReporter : ActionFilterAttribute
+    {
+        private void logValues(RouteData routeData)
+        {
+            Debug.WriteLine("Action Values");
+            foreach(var item in routeData.Values)
+            {
+                Debug.Write("key : ");
+                Debug.WriteLine(item.Key);
+                Debug.Write("value : ");
+                Debug.WriteLine(item.Value);
+            }
+
+        }
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            logValues(filterContext.RouteData);
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
